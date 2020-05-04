4c47ec4e74eb8e9cee1d3284d203011fb6d6224f
Rikard Bartholf <rikard@bartholf.nu>
2020-05-04 09:02:58 +0200

    Set indentation to 4 spaces

--- update-cf.sh
+++ update-cf.sh
@@ -17,25 +17,24 @@ fi
 
 # get the zone id for the requested zone
 ZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$ZONE_NAME&status=active" \
-  -H "X-Auth-Email: $CF_EMAIL" \
-  -H "X-Auth-Key: $CF_TOKEN" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+    -H "X-Auth-Email: $CF_EMAIL" \
+    -H "X-Auth-Key: $CF_TOKEN" \
+    -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
 A_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?name=$ZONE_NAME&type=A" \
-  -H "X-Auth-Email: $CF_EMAIL" \
-  -H "X-Auth-Key: $CF_TOKEN" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id') #  | grep -Po '(?<="id": ")[^"]*')
+    -H "X-Auth-Email: $CF_EMAIL" \
+    -H "X-Auth-Key: $CF_TOKEN" \
+    -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id') #  | grep -Po '(?<="id": ")[^"]*')
 
 RES=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$A_ID" \
-  -H "X-Auth-Email: $CF_EMAIL" \
-  -H "X-Auth-Key: $CF_TOKEN" \
-  -H "Content-Type: application/json" --data "{\"id\":\"$ZONE_ID\",\"type\":\"A\",\"name\":\"$ZONE_NAME\",\"content\":\"$CURR_IP\"}" | jq '.success')
+    -H "X-Auth-Email: $CF_EMAIL" \
+    -H "X-Auth-Key: $CF_TOKEN" \
+    -H "Content-Type: application/json" --data "{\"id\":\"$ZONE_ID\",\"type\":\"A\",\"name\":\"$ZONE_NAME\",\"content\":\"$CURR_IP\"}" | jq '.success')
 
 if [[ $RES  ]]; then
-  echo $CURR_IP > $IP_FILE
-  logger "DDNS New IP $CURR_IP"
-  exit 0
+    echo $CURR_IP > $IP_FILE
+    logger "DDNS New IP $CURR_IP"
+    exit 0
 fi
 
 exit 1
-
